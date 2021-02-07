2e65c5702c05fbf1a5d4c311696e6f58752a7242
Combi2k2 <40012077+Combi2k2@users.noreply.github.com>
2021-02-07 15:55:59 +0700

    Update CrucialData.hpp

--- 2014/CrucialData.hpp
+++ 2014/CrucialData.hpp
@@ -14,21 +14,15 @@ using namespace std;
 
 const int   N   = 1e6 + 5;
 
+typedef pair<int,int>   ii;
+
 vector<int> adj_list[N];
 
-struct Edge {
-    int v;
-    int cost;
-    int point;
-    Edge(int _v = 0,int _c = 0,int _p = 0) : v(_v), cost(_c), point(_p) {}
-}   E[N * 5];
+map<ii,int> index;
 
-int tot = 0;
+int cost[N * 5];
+int point[N * 5];
 
-void addEdge(int u,int v,int time,int length)   {
-    adj_list[u].pb(tot);
-    E[tot++] = Edge(v,time,length);
-}
 int vertices;
 int edges;
 int Time;
